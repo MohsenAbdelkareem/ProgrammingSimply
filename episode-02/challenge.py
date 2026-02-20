2542e792363256394d9f411f4d2b11d6cecb9aed
MohsenAbdelkareem <mohsen.abdelkareem@gmail.com>
2026-02-21 01:24:06 +0200

    E2

---
+++ episode-02/challenge.py
@@ -0,0 +1,29 @@
+print("10" + "20")
+
+print(10 + 20)
+print("10" + "20")
+print("10" * 3)
+
+x = 5
+print(x + x)
+x = "5"
+print(x + x)
+
+
+price = 10.5
+print(type(price))
+
+print(10 / 4)
+
+price = "19.99"
+quantity = 3
+total = price * quantity
+print(total)
+
+print(5 + 2)
+print(5 + 2.0)
+
+print("5" * 3)
+
+
+print("5" * 2.0)
