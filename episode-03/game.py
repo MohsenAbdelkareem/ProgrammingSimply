7331982f2e0af8fd9a9ddff941909df03aec4000
MohsenAbdelkareem <mohsen.abdelkareem@gmail.com>
2026-03-06 19:42:24 +0200

    e3

---
+++ episode-03/game.py
@@ -0,0 +1,28 @@
+# 1- import random library
+import random
+
+# 2- Computer chooses a secret number between 1, 10
+secret_number = random.randint(1, 10)
+
+# 3- we ask the user to enter a guess
+guess_str = input("Guess a number between 1 and 10 : ") # Return string not int
+
+while true:
+# 4- validate inputs
+if guess_str:
+    # logic
+    # 5- Error hanlding
+    try:
+        # 6- Convert guess from string to int
+        guess_num = int(guess_str)
+        # 7- comparison and printing the result
+        if guess_num == secret_number:
+            print(f"Genius! The secret number was {secret_number}. You won!")
+        elif guess_num > secret_number:
+            print("Your guess is too high. Try again.")
+        else:
+            print("Your guess is too low. Try again.")
+    except ValueError:
+        print("Error: Please enter a valid number only")
+else:
+    print("You haven't entered anything. Please enter a number.")
---
+++ episode-03/if_conditions.py
@@ -0,0 +1,24 @@
+# if 0:
+#     print("True")
+# else:
+#     print("False")
+
+# if 5:
+#     print("True")
+
+# if "Hello":
+#     print("True")
+# else:
+#     print("False")
+
+# if "":
+#     print("True")
+# else:
+#     print("False")
+
+age = 16
+if age >= 18:
+    print("Allowed")
+else:
+    print("Not Allowed")
+    
